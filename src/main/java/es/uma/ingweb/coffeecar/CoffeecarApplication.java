2a35bf435eb7b6d40992145d61d709d14138e896
Isolated <isolated@alu-235-117.wifi.uma.es>
2019-11-05 20:27:15 +0100

    first commit

---
+++ src/main/java/es/uma/ingweb/coffeecar/CoffeecarApplication.java
@@ -0,0 +1,13 @@
+package es.uma.ingweb.coffeecar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeecarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeecarApplication.class, args);
+    }
+
+}
