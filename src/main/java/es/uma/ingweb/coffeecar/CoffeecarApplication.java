e071e1d3235209711ebe3c13bc314c81ec4c16a8
Isolated <isolated@determined-hawking.local>
2019-11-11 18:00:38 +0100

    Removed srping security

--- src/main/java/es/uma/ingweb/coffeecar/CoffeecarApplication.java
+++ src/main/java/es/uma/ingweb/coffeecar/CoffeecarApplication.java
@@ -1,12 +1,13 @@
 package es.uma.ingweb.coffeecar;
 
+import es.uma.ingweb.coffeecar.entities.Person;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CoffeecarApplication {
-
     public static void main(String[] args) {
+        Person person = new Person();
         SpringApplication.run(CoffeecarApplication.class, args);
     }
 
