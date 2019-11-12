c57ae399038419f917f69304679e5bb30d0434d8
guisanpea <guisanpea@gmail.com>
2019-11-12 19:28:42 +0100

    Changed dependencies

--- src/main/java/es/uma/ingweb/coffeecar/CoffeecarApplication.java
+++ src/main/java/es/uma/ingweb/coffeecar/CoffeecarApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CoffeecarApplication {
     public static void main(String[] args) {
         Person person = new Person();
+        person.getFirstName();
         SpringApplication.run(CoffeecarApplication.class, args);
     }
 
