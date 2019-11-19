ebfcc8028a863fc6bc0f505feeebd91956d9dfe9
guisanpea <guisanpea@gmail.com>
2019-11-19 19:58:37 +0100

    Renamed Entities

---
+++ src/main/java/es/uma/ingweb/coffeecar/entities/User.java
@@ -0,0 +1,25 @@
+package es.uma.ingweb.coffeecar.entities;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private String password;
+    private String mail;
+    @OneToMany(mappedBy = "driver", cascade = CascadeType.ALL)
+    @ToString.Exclude @EqualsAndHashCode.Exclude
+    private List<Announce> ownedAnnounces;
+    @ManyToMany(mappedBy = "passengers",cascade = CascadeType.ALL)
+    @ToString.Exclude @EqualsAndHashCode.Exclude
+    private List<Announce> joinedAnnounces;
+}
---
+++ src/main/java/es/uma/ingweb/coffeecar/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package es.uma.ingweb.coffeecar.repositories;
+
+import java.util.List;
+
+import es.uma.ingweb.coffeecar.entities.User;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "User", path = "users")
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+}
