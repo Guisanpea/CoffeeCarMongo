39eca759ff35f337efac1c914494df9be172752e
JesusMarinGarcia <jesus1998mg@uma.es>
2019-11-26 18:25:33 +0100

    adding new queries to AnnounceRepository

--- src/main/java/es/uma/ingweb/coffeecar/repositories/AnnounceRepository.java
+++ src/main/java/es/uma/ingweb/coffeecar/repositories/AnnounceRepository.java
@@ -5,11 +5,16 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Date;
 import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "Announce", path = "announced")
 public interface AnnounceRepository extends PagingAndSortingRepository<Announce, Long> {
 
-    List<Announce> findByDriver(@Param("driver") User driver);
-    List<Announce> findByPassengers(@Param("pasengers") User passenger);
+    List<Announce> findAnnouncesByDriver(@Param("driver") User driver);
+    List<Announce> findAnnouncesByPassengers(@Param("pasengers") User passenger);
+    List<Announce> findAnnouncesByDriverNotAndPassengersNotContaining(@Param("user") User user, @Param("user") User passenger);
+    List<Announce> findAnnouncesByArrival(@Param("arrival")String arrival);
+    List<Announce> findAnnouncesByArrivalAAndArrivalDate(@Param("arrival")String arrival,@Param("arrivalDate") Date arrivalDate);
+    List<Announce> findAnnouncesByArrivalDate(@Param("arrival")Date arrivalDate);
 }
