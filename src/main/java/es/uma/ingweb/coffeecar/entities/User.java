0e03ab58b22bc3dec46abb2b3b5c64d6848e4230
JavierC07 <36677101+JavierC07@users.noreply.github.com>
2019-12-01 15:08:38 +0100

    id

--- src/main/java/es/uma/ingweb/coffeecar/entities/User.java
+++ src/main/java/es/uma/ingweb/coffeecar/entities/User.java
@@ -10,11 +10,12 @@ import java.util.List;
 @Entity
 @Data
 public class User {
-    @Id
+
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String name;
     private String password;
+    @Id
     private String mail;
     @OneToMany(mappedBy = "driver", cascade = CascadeType.ALL)
     @ToString.Exclude @EqualsAndHashCode.Exclude
