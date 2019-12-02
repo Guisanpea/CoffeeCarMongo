2cacb4d4f4c18b12fb97ed18d8c52f68eeb3f298
JavierC07 <36677101+JavierC07@users.noreply.github.com>
2019-12-02 13:53:46 +0100

    Query nueva

--- src/main/java/es/uma/ingweb/coffeecar/repositories/AnnounceRepository.java
+++ src/main/java/es/uma/ingweb/coffeecar/repositories/AnnounceRepository.java
@@ -1,20 +1,26 @@
 package es.uma.ingweb.coffeecar.repositories;
 
 import es.uma.ingweb.coffeecar.entities.*;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "Announce", path = "announced")
 public interface AnnounceRepository extends PagingAndSortingRepository<Announce, Long> {
 
-    List<Announce> findAnnouncesByDriver(@Param("driver") User driver);
-    List<Announce> findAnnouncesByPassengers(@Param("pasengers") User passenger);
-    List<Announce> findAnnouncesByDriverNotAndPassengersNotContaining(@Param("user") User user, @Param("user") User passenger);
+    List<Announce> findAnnouncesByDriver_Mail(@Param("mail") String mail);
+    @Query(value = "Select a from Announce a, a.passengers where p.mail like concat('%',:mail,'%')", nativeQuery = true)
+    List<Announce> findAnnouncesByPassengers(@Param("mail") String mail);
+    @Query(value = "Select a from Announce a, a.passengers p where p.mail not like concat('%',:mail_passenger,'%') and a.driver.mail " +
+            "not like concat('%',:driver_mail,'%')", nativeQuery = true)
+    List<Announce> findAnnouncesByDriverNotAndPassengersNot(@Param("driver_mail") String driver_mail, @Param("mail_passenger") String mail_passenger);
     List<Announce> findAnnouncesByArrival(@Param("arrival")String arrival);
-    List<Announce> findAnnouncesByArrivalAndArrivalDate(@Param("arrival")String arrival,@Param("arrivalDate") Date arrivalDate);
-    List<Announce> findAnnouncesByArrivalDate(@Param("arrival")Date arrivalDate);
+    List<Announce> findAnnouncesByArrivalAndArrivalDate(String arrival, LocalDateTime arrivalDate);
+    List<Announce> findAnnouncesByArrivalDate(LocalDateTime arrivalDate);
 }
