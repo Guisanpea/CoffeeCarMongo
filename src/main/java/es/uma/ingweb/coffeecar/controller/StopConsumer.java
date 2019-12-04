c301b825ef9c51c8ae84bb2654c65aad697a4f9f
JulioMh <kilojulio@gmail.com>
2019-12-04 19:40:46 +0100

    Update StopConsumer.java

--- src/main/java/es/uma/ingweb/coffeecar/controller/StopConsumer.java
+++ src/main/java/es/uma/ingweb/coffeecar/controller/StopConsumer.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
