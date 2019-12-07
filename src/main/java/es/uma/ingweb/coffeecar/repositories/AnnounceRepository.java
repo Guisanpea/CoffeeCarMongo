32ee0c4ee30208fac2d56644ca627398a3caf371
Guisanpea <guisanpea@gmail.com>
2019-12-07 15:12:05 +0100

    New queries

--- src/main/java/es/uma/ingweb/coffeecar/repositories/AnnounceRepository.java
+++ src/main/java/es/uma/ingweb/coffeecar/repositories/AnnounceRepository.java
@@ -17,4 +17,16 @@ public interface AnnounceRepository extends PagingAndSortingRepository<Announce,
     List<Announce> findAnnouncesByArrival(@Param("arrival")String arrival);
     List<Announce> findAnnouncesByArrivalAndArrivalDate(@Param("arrival") String arrival, @Param("arrival") String arrivalDate);
     List<Announce> findAnnouncesByArrivalDate(@Param("arrivalDate") String arrivalDate);
+    List<Announce> findAnnouncesByDriverEmail(@Param("email") String driverEmail);
+    List<Announce> findAnnouncesByDriverEmailNot(@Param("email") String driverEmail);
+    @Query("select a from Announce a left join a.passengers u where u.email = ?1")
+    List<Announce> findByPassengersEmails(String email);
+    @Query("select distinct a from Announce a " +
+          "left join a.passengers p left join a.driver d " +
+          "where p.email <> ?1 and d.email <> ?1")
+    List<Announce> findAvailableAnnounces(String email);
+    @Query("select distinct a from Announce a " +
+          "left join a.passengers p left join a.driver d " +
+          "where p.email <> ?1 and d.email <> ?1")
+    List<Announce> findUserTrips(String email);
 }
