c9dcafb172df93ce8fbadcb8cf4fe097dde6c2d7
Dinohaurio <kilojulio@gmail.com>
2019-12-10 20:17:25 +0100

    Nombres bonitos

--- src/main/java/es/uma/ingweb/coffeecar/controller/BusConsumer.java
+++ src/main/java/es/uma/ingweb/coffeecar/controller/BusConsumer.java
@@ -21,14 +21,14 @@ public class BusConsumer {
         this.restTemplate = restTemplate;
     }
 
-    @GetMapping(value="/")
+    @GetMapping(value="")
     public List<BusHierarchy.BusInfoResponse.BusData> getBusesPosition(){
         String uri = "http://datosabiertos.malaga.eu/api/3/action/datastore_search?resource_id=9bc05288-1c11-4eec-8792-d74b679c8fcf";
 
         return Objects.requireNonNull(restTemplate.getForObject(uri, BusHierarchy.class)).getResult().getBusesData();
     }
 
-    @GetMapping(value="/byLine")
+    @GetMapping(value="/search/findByLine")
     public List<BusHierarchy.BusInfoResponse.BusData> getBusesPosition(@RequestParam(name="line") float line){
         String uri = "http://datosabiertos.malaga.eu/api/3/action/datastore_search?resource_id=9bc05288-1c11-4eec-8792-d74b679c8fcf";
         List<BusHierarchy.BusInfoResponse.BusData> buses =
